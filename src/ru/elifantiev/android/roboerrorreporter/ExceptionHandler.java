c1be689b855f44f3a14cc2e369a7d6aa4d877492
Olegas <oleg@elifantiev.ru>
2011-05-03 21:36:24 +0400

    Initial commit

---
+++ src/ru/elifantiev/android/roboerrorreporter/ExceptionHandler.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2011 Oleg Elifantiev
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.elifantiev.android.roboerrorreporter;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+final class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+    private final DateFormat formatter = new SimpleDateFormat("dd.MM.yy HH:mm");
+    private final DateFormat fileFormatter = new SimpleDateFormat("dd-MM-yy");
+    private final String stacktraceDir;
+    private final Thread.UncaughtExceptionHandler previousHandler;
+
+    private ExceptionHandler(Context context) {
+        previousHandler = Thread.getDefaultUncaughtExceptionHandler();
+        stacktraceDir = String.format("/Android/data/%s/files/", context.getPackageName());
+    }
+
+    static ExceptionHandler inContext(Context context) {
+        return new ExceptionHandler(context);
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable exception) {
+        final String state = Environment.getExternalStorageState();
+        final Date dumpDate = new Date(System.currentTimeMillis());
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+
+            StringBuilder reportBuilder = new StringBuilder();
+            reportBuilder
+                    .append("\n\n\n")
+                    .append(formatter.format(dumpDate))
+                    .append("\n")
+                    .append(thread.toString())
+                    .append("\n");
+            processThrowable(exception, reportBuilder);
+
+            File sd = Environment.getExternalStorageDirectory();
+            File stacktrace = new File(
+                    sd.getPath() + stacktraceDir,
+                    String.format(
+                            "stacktrace-%s.txt",
+                            fileFormatter.format(dumpDate)));
+            File dumpdir = stacktrace.getParentFile();
+            boolean dirReady = dumpdir.isDirectory() || dumpdir.mkdirs();
+            if (dirReady) {
+                FileWriter writer = null;
+                try {
+                    writer = new FileWriter(stacktrace, true);
+                    writer.write(reportBuilder.toString());
+                } catch (IOException e) {
+                    // ignore
+                } finally {
+                    try {
+                        if (writer != null)
+                            writer.close();
+                    } catch (IOException e) {
+                        // ignore
+                    }
+                }
+            }
+        }
+        if(previousHandler != null)
+            previousHandler.uncaughtException(thread, exception);
+    }
+
+    private void processThrowable(Throwable exception, StringBuilder builder) {
+        if(exception == null)
+            return;
+        StackTraceElement[] stackTraceElements = exception.getStackTrace();
+        builder
+                .append("Exception: ").append(exception.getClass().getName()).append("\n")
+                .append("Message: ").append(exception.getMessage()).append("\nStacktrace:\n");
+        for(StackTraceElement element : stackTraceElements) {
+            builder.append("\t").append(element.toString()).append("\n");
+        }
+        processThrowable(exception.getCause(), builder);
+    }
+}
